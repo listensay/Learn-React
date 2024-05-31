b948d597f3e8d445f82850931aa125926c2b1848
Miliya <odr233@gmail.com>
2024-05-31 10:37:21 +0800

    react css

---
+++ 04_learn_reactcss/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './01_内联样式的css/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
